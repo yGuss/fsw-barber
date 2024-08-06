966dc8367e422ed9e3d4bc86ec1cfd3a2179dab0
ygus <guuhhenrique159@gmail.com>
2024-08-06 17:33:45 -0300

    feat: add quick search

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { SearchIcon } from "lucide-react"
+import { EyeIcon, FootprintsIcon, SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
@@ -11,6 +11,11 @@ import BarbershopItem from "./_components/barbershop-item"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
   return (
     <div>
       <Header />
@@ -18,6 +23,7 @@ const Home = async () => {
         {/* TEXTO */}
         <h2 className="text-xl font-bold">Olá, Gustavo!</h2>
         <p>Terça-Feira, 06 de agosto.</p>
+
         {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
           {/* md:mx-[20%] md:w-[50%] */}
@@ -25,6 +31,49 @@ const Home = async () => {
           <Button>
             <SearchIcon />
           </Button>
+        </div>
+
+        {/* BUSCA RÁPIDA */}
+        <div className="mt-6 flex flex-row gap-2 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <div className="flex gap-3">
+            <Button className="gap-2" variant="secondary">
+              <Image alt="Cabelo" src="/cabelo.svg" width={16} height={16} />
+              Cabelo
+            </Button>
+          </div>
+
+          <div className="flex gap-3">
+            <Button className="gap-2" variant="secondary">
+              <Image alt="Barba" src="/barba.svg" width={16} height={16} />
+              Barba
+            </Button>
+          </div>
+
+          <div className="flex gap-3">
+            <Button className="gap-2" variant="secondary">
+              <Image
+                alt="Acabamento"
+                src="/acabamento.svg"
+                width={16}
+                height={16}
+              />
+              Acabamento
+            </Button>
+          </div>
+
+          <div className="flex gap-3">
+            <Button className="gap-2" variant="secondary">
+              <FootprintsIcon width={16} height={16} />
+              Pézinho
+            </Button>
+          </div>
+
+          <div className="flex gap-3">
+            <Button className="gap-2" variant="secondary">
+              <EyeIcon width={16} height={16} />
+              Sobrancelha
+            </Button>
+          </div>
         </div>
         {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full rounded-xl">
@@ -62,6 +111,7 @@ const Home = async () => {
           </CardContent>
         </Card>
 
+        {/* RECOMENDADOS */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
@@ -70,7 +120,27 @@ const Home = async () => {
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        {/* POPULARES */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+
+      <footer>
+        <Card className="px-5 py-6 text-center">
+          <CardContent>
+            <p className="text-sm text-gray-400">
+              © 2024 Copyright <span className="font-bold">FSW Barber</span>{" "}
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
