278536673ee72f47453b77106839d1b508c1d9d2
ygus <guuhhenrique159@gmail.com>
2024-08-06 21:57:45 -0300

    feat: ensure barbershop services are show on barbershop page

--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -1,9 +1,11 @@
+import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { notFound } from "next/navigation"
+
 interface BarbershopPageProps {
   params: {
     id: string
@@ -11,15 +13,20 @@ interface BarbershopPageProps {
 }
 
 const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  // chamar o banco de dados
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
+    },
+    include: {
+      services: true,
     },
   })
 
   if (!barbershop) {
     return notFound()
   }
+
   return (
     <div>
       {/* IMAGEM */}
@@ -66,6 +73,15 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
+      </div>
+
+      <div className="space-y-3 p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   )
