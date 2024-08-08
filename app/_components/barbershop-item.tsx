98460a9a20ee0d8f1845a5f9333119c0fcfdcfc7
ygus <guuhhenrique159@gmail.com>
2024-08-08 12:40:06 -0300

    feat: ensure user can search for barbershops

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -35,7 +35,7 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
           <Button variant="secondary" className="mt-3 w-full" asChild>
-            <Link href={`/barbershop/${barbershop.id}`}>Reservar</Link>
+            <Link href={`/barbershops/${barbershop.id}`}>Reservar</Link>
           </Button>
         </div>
       </CardContent>
