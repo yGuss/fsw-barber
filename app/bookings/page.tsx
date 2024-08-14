c87940915ed35bc8125f2b0fea73ae6bdc7dc292
ygus <guuhhenrique159@gmail.com>
2024-08-14 16:09:47 -0300

    chore: added Message on booking appointments with user is not logged

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,16 +1,29 @@
 import { getServerSession } from "next-auth"
 import Header from "../_components/header"
+
 import { authOptions } from "../_lib/auth"
-import { notFound } from "next/navigation"
+
 import BookingItem from "../_components/booking-item"
 import { getConfirmedBookings } from "../_data/get-confirmed-bookings"
 import { getConcludedBookings } from "../_data/get-concluded-bookings"
+import { Button } from "../_components/ui/button"
+import Link from "next/link"
 
 const Bookings = async () => {
   const session = await getServerSession(authOptions)
-  if (!session?.user) {
-    return notFound()
-  }
+  if (!session?.user)
+    return (
+      <div className="mt-6 flex flex-col items-center justify-center gap-4 p-5 text-center">
+        <h1 className="text-xl font-bold">Agendamentos</h1>
+        <p className="text-gray-400">Você não tem agendamentos.</p>
+        <Button asChild>
+          <Link href="/" className="min-w-[40%] max-w-[40%]">
+            Voltar ao menu inicial
+          </Link>
+        </Button>
+      </div>
+    )
+
   const confirmedBookings = await getConfirmedBookings()
   const concludedBookings = await getConcludedBookings()
   return (
